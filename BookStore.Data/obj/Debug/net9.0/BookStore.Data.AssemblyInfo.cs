b3272ac5e7cd1796093de6387ae4471b1a872e6c
fatemeh imani <imanifateimani1984@gmail.com>
2025-09-26 01:17:24 +0330

    User

--- BookStore.Data/obj/Debug/net9.0/BookStore.Data.AssemblyInfo.cs
+++ BookStore.Data/obj/Debug/net9.0/BookStore.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a964f2704c57536e3d1fa88dcd95747cc0065f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd67275f79be399c8015dc336ea2cad7d10a0f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
